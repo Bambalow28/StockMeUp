2f32c51c99a8a790e7ab34b141a5e02f06501aee
Joshua Alanis <joshalanis28@gmail.com>
2022-02-11 23:43:52 -0500

    Created Basic UI for Portfolio Info

--- lib/stocksPage/portfolioInfo.dart
+++ lib/stocksPage/portfolioInfo.dart
@@ -6,6 +6,12 @@ import 'package:webview_flutter/webview_flutter.dart';
 class PortfolioInfo extends StatefulWidget {
   @override
   _PortfolioInfo createState() => _PortfolioInfo();
+
+  int portfolioCount;
+  List portfolioName;
+  PortfolioInfo(
+      {Key? key, required this.portfolioCount, required this.portfolioName})
+      : super(key: key);
 }
 
 //View News Widget State
@@ -44,6 +50,31 @@ class _PortfolioInfo extends State<PortfolioInfo> {
           title: Text(appBarTitle),
           backgroundColor: const Color.fromRGBO(38, 38, 38, 1.0),
           automaticallyImplyLeading: true,
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: IconButton(
+                  icon: Icon(
+                    Icons.add_circle_outline_rounded,
+                    color: Colors.blue[300],
+                  ),
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.grey[850],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30.0),
+                                topRight: Radius.circular(30.0))),
+                        builder: (BuildContext context) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[],
+                          );
+                        });
+                  }),
+            )
+          ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: const Color.fromRGBO(24, 24, 24, 1.0),
@@ -81,21 +112,57 @@ class _PortfolioInfo extends State<PortfolioInfo> {
         ),
         body: GestureDetector(
             onTap: () => {FocusScope.of(context).unfocus()},
-            child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color.fromRGBO(23, 23, 23, 1),
-                      Color.fromRGBO(13, 13, 13, 1)
-                    ],
+            child: SingleChildScrollView(
+              child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color.fromRGBO(23, 23, 23, 1),
+                        Color.fromRGBO(13, 13, 13, 1)
+                      ],
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: <Widget>[],
-                ))));
+                  child: ListView.builder(
+                      itemCount: widget.portfolioCount,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 200.0,
+                          margin: EdgeInsets.only(bottom: 20.0),
+                          decoration: BoxDecoration(
+                              color: Colors.grey[850],
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0))),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(top: 10.0, left: 20.0),
+                                    child: Text(
+                                      widget.portfolioName[index],
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: SizedBox(),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      })),
+            )));
   }
 }
